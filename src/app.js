1bfae90e212e68a906bc51a8b6da80ce8c4f5366
Developer <bhargavmgokani@gmail.com>
2026-01-22 00:36:54 +0530

    final changes

--- src/app.js
+++ src/app.js
@@ -16,7 +16,8 @@ const app = express();
 app.use(helmet());
 app.use(
   cors({
-    origin: "*",
+    origin: true, // allow requests, safe for Flutter
+    credentials: true,
   })
 );
 app.use(express.json());
